90c7056edda6571a46af40992302b9d44c9710b7
ikethecoder <ikethecoder@copeconsulting.ca>
2024-07-25 22:16:25 -0700

    add geowebcache image build

---
+++ docker/gwc/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-core-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-core-context.xml
+envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml
+
+# envsubst </overrides/cache/geowebcache.xml > /geowebcache/cache/geowebcache.xml
+
+catalina.sh "run"
