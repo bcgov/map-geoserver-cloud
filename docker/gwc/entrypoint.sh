c1c91d135b0748c023b24be804d9f495984fde2a
ikethecoder <ikethecoder@copeconsulting.ca>
2024-07-26 00:10:09 -0700

    do envsubst for gscloud geowebcache

--- docker/gwc/entrypoint.sh
+++ docker/gwc/entrypoint.sh
@@ -3,6 +3,6 @@
 envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-core-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-core-context.xml
 envsubst </overrides/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml > /usr/local/tomcat/webapps/gwc/WEB-INF/geowebcache-wmsservice-context.xml
 
-# envsubst </overrides/cache/geowebcache.xml > /geowebcache/cache/geowebcache.xml
+envsubst </overrides/cache/geowebcache.xml > /geowebcache/cache/geowebcache.xml
 
 catalina.sh "run"
