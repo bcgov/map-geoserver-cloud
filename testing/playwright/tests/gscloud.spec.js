3e074fa2d6e205766746bb63a386040767cc3ec3
Michael Nelson <michael.nelson@gov.bc.ca>
2026-09-08 10:56:04 -0700

    Updates to gscloud.spec tests (MAP-8977)
    * Updates to gscloud.spec tests
    
    * Remove commented out lines

--- testing/playwright/tests/gscloud.spec.js
+++ testing/playwright/tests/gscloud.spec.js
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
-import requests from "../fixtures/wms-map-requests.json";
-import gcRequests from "../fixtures/wms-get-capabilities-requests.json";
+import wmsMapRequests from "../fixtures/wms-map-requests.json";
+import wmsGetCapRequests from "../fixtures/wms-get-capabilities-requests.json";
+import wfsFeatureRequests from "../fixtures/wfs-feature-requests.json";
 
 let getLcParameterValue = function (param, searchParams) {
 
@@ -27,55 +28,6 @@ test.describe(envDescription + " routes", () => {
 
     // This query string has the "layers" parameter.
     let queryString = "?STYLES=&layers=pub%3AWHSE_LEGAL_ADMIN_BOUNDARIES.ABMS_COUNTIES_SP&WIDTH=400&service=wms&FORMAT=image%2Fpng&request=getmap&HEIGHT=266&SRS=EPSG%3A3005&version=1.1.1&BBOX=794280.590063033%2C264941.75762325%2C1636273.15489024%2C888458.117582758";
-    let noLayerQueryString = "?STYLES=&WIDTH=400&service=wms&FORMAT=image%2Fpng&request=getmap&HEIGHT=266&SRS=EPSG%3A3005&version=1.1.1&BBOX=794280.590063033%2C264941.75762325%2C1636273.15489024%2C888458.117582758";
-
-    test("/geo/pub/[layername]/wms", async ({ page }) => {
-
-        let path = "/geo/pub/WHSE_LEGAL_ADMIN_BOUNDARIES.ABMS_COUNTIES_SP/wms"
-        let requestUrl = domain + path + queryString;
-    
-        let response = await page.goto(requestUrl);
-        await expect(response.status()).toBe(200);
-        await expect(page).toHaveScreenshot();
-    });
-
-    test("/geo/pub/[layername]/ows", async ({ page }) => {
-
-        let path = "/geo/pub/WHSE_LEGAL_ADMIN_BOUNDARIES.ABMS_COUNTIES_SP/ows"
-        let requestUrl = domain + path + queryString;
-    
-        let response = await page.goto(requestUrl);
-        await expect(response.status()).toBe(200);
-        await expect(page).toHaveScreenshot();
-    });
-
-    test("/geo/pub/[layername]/wms NO &layers=", async ({ page }) => {
-
-        let path = "/geo/pub/WHSE_LEGAL_ADMIN_BOUNDARIES.ABMS_COUNTIES_SP/wms"
-        let requestUrl = domain + path + noLayerQueryString;
-        
-        try {
-            let response = await page.goto(requestUrl);
-            await expect(response.status()).toBe(200);
-        }
-        catch (error) {
-            expect(error.message).toContain("Download is starting");
-        }
-    });
-
-    test("/geo/pub/[layername]/ows NO &layers=", async ({ page }) => {
-
-        let path = "/geo/pub/WHSE_LEGAL_ADMIN_BOUNDARIES.ABMS_COUNTIES_SP/ows"
-        let requestUrl = domain + path + noLayerQueryString;
-
-        try {
-            let response = await page.goto(requestUrl);
-            await expect(response.status()).toBe(200);
-        }
-        catch (error) {
-            expect(error.message).toContain("Download is starting");
-        }
-    });
 
     test("/geo/pub/wms", async ({ page }) => {
 
@@ -118,9 +70,41 @@ test.describe(envDescription + " routes", () => {
     });
 });
 
+test.describe(envDescription + " WFS GetFeature", () => {
+
+    wfsFeatureRequests.forEach((requestUrl, index) => {
+
+        requestUrl = domain + "/" + requestUrl;
+        let url = new URL(requestUrl);
+        let path = url.pathname;
+        let href = url.href;
+
+        test(index, 
+            { 
+                annotation : [
+                    { 
+                        type: "path", 
+                        description : path
+                    },
+                    {
+                        type: "href", 
+                        description : href
+                    }
+                ]
+            },
+            async ({ page }) => {
+
+            let response = await page.goto(requestUrl);
+            expect(response.status()).toBe(200);
+            let contentType = response.headers()["content-type"];
+            expect(contentType).toContain("application/json");
+        });
+    });
+});
+
 test.describe(envDescription + " WMS GetMap", () => {
 
-    requests.forEach((requestUrl, index) => {
+    wmsMapRequests.forEach((requestUrl, index) => {
 
         requestUrl = domain + "/" + requestUrl;
         let url = new URL(requestUrl);
@@ -163,9 +147,9 @@ test.describe(envDescription + " WMS GetMap", () => {
     });
 });
 
-test.describe(envDescription + "WMS GetCapabilities", () => {
+test.describe(envDescription + " WMS GetCapabilities", () => {
 
-    gcRequests.forEach((requestUrl, index) => {
+    wmsGetCapRequests.forEach((requestUrl, index) => {
 
         requestUrl = domain + "/" + requestUrl;
         let url = new URL(requestUrl);
@@ -217,29 +201,13 @@ test.describe(envDescription + " Case sensitivity", () => {
 
     test("GSC: request param is NOT case sensitive", async ({ page }) => {
 
-        let requestUrl = domain + "/test/geo/pub/wms?=&service=WMS&REQUEST=GetMap&layers=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
-        let response = await page.goto(requestUrl);
-
-        expect(response.status()).toBe(200);
-    });
-
-    test("GSC: request value is NOT case sensitive", async ({ page }) => {
-
-        let requestUrl = domain + "/test/geo/pub/wms?=&service=WMS&request=GETMAP&layers=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
-        let response = await page.goto(requestUrl);
-
-        expect(response.status()).toBe(200);
-    });
-
-    test("GWC: request param is NOT case sensitive", async ({ page }) => {
-
         let requestUrl = domain + "/geo/pub/wms?=&service=WMS&REQUEST=GetMap&layers=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
         let response = await page.goto(requestUrl);
 
         expect(response.status()).toBe(200);
     });
 
-    test("GWC: request value is NOT case sensitive", async ({ page }) => {
+    test("GSC: request value is NOT case sensitive", async ({ page }) => {
 
         let requestUrl = domain + "/geo/pub/wms?=&service=WMS&request=GETMAP&layers=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
         let response = await page.goto(requestUrl);
@@ -249,22 +217,10 @@ test.describe(envDescription + " Case sensitivity", () => {
 
     test("GSC: layers param is NOT case sensitive", async ({ page }) => {
 
-        let requestUrl = domain + "/test/geo/pub/wms?=&service=WMS&request=GetMap&LAYERS=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
+        let requestUrl = domain + "/geo/pub/wms?=&service=WMS&request=GetMap&LAYERS=pub:WHSE_HUMAN_CULTURAL_ECONOMIC.EMRG_ORDER_AND_ALERT_AREAS_SP&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
         let response = await page.goto(requestUrl);
 
         expect(response.status()).toBe(200);
-    });
-
-    test("GSC: layers value IS case sensitive", async ({ page }) => {
-
-        let requestUrl = domain + "/test/geo/pub/wms?=&service=WMS&request=GetMap&layers=pub:whse_human_cultural_economic.emrg_order_and_alert_areas_sp&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
-
-        try {
-            await page.goto(requestUrl);
-        }
-        catch (error) {
-            expect(error.message).toContain("Download is starting");
-        }
     });
 
     test("GWC: layers param is NOT case sensitive", async ({ page }) => {
@@ -273,18 +229,6 @@ test.describe(envDescription + " Case sensitivity", () => {
         let response = await page.goto(requestUrl);
 
         expect(response.status()).toBe(200);
-    });
-
-    test("GWC: layers value IS case sensitive", async ({ page }) => {
-
-        let requestUrl = domain + "/geo/pub/wms?=&service=WMS&request=GetMap&layers=pub:whse_human_cultural_economic.emrg_order_and_alert_areas_sp&styles=&format=image/png&transparent=true&version=1.1.0&width=256&height=256&srs=EPSG:4326&tiled=true&bbox=-120.234375,49.921875,-120.05859375,50.09765625";
-
-        try {
-            await page.goto(requestUrl);
-        }
-        catch (error) {
-            expect(error.message).toContain("Download is starting");
-        }
     });
 });
 
